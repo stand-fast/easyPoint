cd859a8fb43a2073c4ccce9ffcf0d2daaaa274a3
stand-fast <1553162002@qq.com>
2019-07-20 12:34:25 +0800

    修改了Account,增加了两行输出

--- src/main/java/com/easyPoint/pojo/Account.java
+++ src/main/java/com/easyPoint/pojo/Account.java
@@ -8,5 +8,9 @@ public class Account {
     public static void main(String[] args) {
         System.out.println("大家好");
         System.out.println("大家好");
+        System.out.println("大家好");
+        System.out.println("大家好");
+        System.out.println("大家好");
+        System.out.println("大家好");
     }
 }
