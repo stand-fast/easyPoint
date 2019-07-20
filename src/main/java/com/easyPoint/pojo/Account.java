60318a91fd2e1b1dbff981b78ea66e242a4d5c6a
stand-fast <1553162002@qq.com>
2019-07-20 11:32:15 +0800

    创建项目

---
+++ src/main/java/com/easyPoint/pojo/Account.java
@@ -0,0 +1,11 @@
+package com.easyPoint.pojo;
+
+/**
+ * @author FHJ
+ * @date 2019/7/20 11:31
+ */
+public class Account {
+    public static void main(String[] args) {
+        System.out.println("大家好");
+    }
+}
