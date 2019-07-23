71f4acbe1983c3d6dde7739abb8e77b51f669bb2
stand-fast <45225013+stand-fast@users.noreply.github.com>
2019-07-23 22:34:48 +0800

    Update Account.java

--- src/main/java/com/easyPoint/pojo/Account.java
+++ src/main/java/com/easyPoint/pojo/Account.java
@@ -6,28 +6,6 @@ package com.easyPoint.pojo;
  */
 public class Account {
     public static void main(String[] args) {
-        System.out.println("大家好");
-        System.out.println("大家好");
-        System.out.println("大家好");
-        System.out.println("大家好");
-        System.out.println("大家好");
-        System.out.println("大家好");
-        System.out.println("大家好");
-        System.out.println("大家好");
-        System.out.println("大家好");
-        System.out.println("大家好");
-        System.out.println("你们好啊啊啊啊");
-        System.out.println("你们好啊啊啊啊");
-        System.out.println("你们好啊啊啊啊");
-        System.out.println("你们好啊啊啊啊");
-        System.out.println("你们好啊啊啊啊");
-        System.out.println("你们好啊啊啊啊");
-        System.out.println("你们好啊啊啊啊");
-        System.out.println("中华人民共和国");
-        System.out.println("中华人民共和国");
-        System.out.println("中华人民共和国");
-        System.out.println("中华人民共和国");
-        System.out.println("中华人民共和国");
-        System.out.println("中华人民共和国");
+        
     }
 }
