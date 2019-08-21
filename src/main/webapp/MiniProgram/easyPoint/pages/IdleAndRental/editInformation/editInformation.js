18bcbadd7ef867ecc483c64ed97ab595d0a1e890
songtown <35566102+songtown@users.noreply.github.com>
2019-08-21 13:43:56 +0800

    添加租赁订单顾客信息编辑页面

---
+++ src/main/webapp/MiniProgram/easyPoint/pages/IdleAndRental/editInformation/editInformation.js
@@ -0,0 +1,79 @@
+// pages/IdleAndRental/editInformation/editInformation.js
+Page({
+
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        name:"广金吴彦祖",
+        phone:"12345678910"
+    },
+    clearName:function(e){
+        this.setData({
+            name:""
+        })
+    },
+    clearPhone: function (e) {
+        this.setData({
+            phone: ""
+        })
+    },
+    formSubmit:function(){
+        
+    },
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function (options) {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function () {
+
+    },
+
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function () {
+
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function () {
+
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function () {
+
+    }
+})
