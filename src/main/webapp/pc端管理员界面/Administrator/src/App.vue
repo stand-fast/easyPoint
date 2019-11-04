dedd9c188cc6499566ad2f6895493a7dfb768984
Hjq <1037765897@qq.com>
2019-11-05 01:07:50 +0800

    管理员板块优化

---
+++ src/main/webapp/pc端管理员界面/Administrator/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <model />
+    <router-view />
+  </div>
+</template>
+
+<script>
+import model from "./components/model.vue";
+export default {
+  name: "app",
+  components: {
+    model
+  }
+};
+</script>
+
+<style>
+@import url(./assets/public.css);
+</style>
