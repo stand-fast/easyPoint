577c53eaf3f33aad000703a83f2b43d783eca7e9
stand-fast <1553162002@qq.com>
2019-11-14 10:37:18 +0800

    Merge branch 'release' of https://github.com/stand-fast/easyPoint into release
    # Conflicts:
    #	.idea/workspace.xml
    #	pom.xml
    #	src/main/resources/springMVC.xml
    #	target/classes/com/easyPoint/interceptor/TokenIntercptor.class
    #	target/easyPoint.war
    #	target/easyPoint/WEB-INF/classes/com/easyPoint/interceptor/TokenIntercptor.class

--- src/main/java/com/easyPoint/controller/administrator/travel/AssociationController.java
+++ src/main/java/com/easyPoint/controller/administrator/travel/AssociationController.java
@@ -8,8 +8,13 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * 同乡会控制器
+ *
  * @author FHJ
  * @date 2019/11/10 18:46
  */
@@ -23,10 +28,11 @@ public class AssociationController {
 
     /**
      * 添加同乡会
+     *
      * @param associationName
      * @return
      */
-    @RequestMapping("/addAssociation")
+    @RequestMapping(value = "/addAssociation")
     @ResponseBody
     public Result addAssociation(String associationName) {
         Result result = new Result();
@@ -58,5 +64,141 @@ public class AssociationController {
             result.setMessage("同乡会添加失败！");
             return result;
         }
+    }
+
+    /**
+     * 添加同乡会上下车地点
+     *
+     * @param associationId
+     * @param place
+     * @return
+     */
+    @RequestMapping(value = "/addAssociationPlace")
+    @ResponseBody
+    public Result addAssociationPlace(String associationId, String place) {
+        Result result = new Result();
+
+        if (associationId == null || associationId.equals("") || place == null || place.equals("")) {
+            result.setCode(0);
+            result.setMessage("参数为空！");
+            return result;
+        }
+
+        // 查看同乡会是否存在
+        Integer associationNum = associationService.findAssociationById(associationId);
+
+        if (associationNum == 0) {
+            result.setCode(-2);
+            result.setMessage("该同乡会不存在！");
+            return result;
+        }
+
+        // 查看该同乡会是否添加过该地址
+        Integer placeNum = associationService.findPlaceByIdAndPlace(associationId, place);
+
+        if (placeNum != 0) {
+            result.setCode(-3);
+            result.setMessage("此同乡会已经添加过该地址了！");
+            return result;
+        }
+
+        // 添加地址
+        Integer flag = associationService.addPlace(associationId, place);
+
+        if (flag == 1) {
+            result.setCode(1);
+            result.setMessage("添加成功！");
+            return result;
+        } else {
+            result.setCode(-1);
+            result.setMessage("添加失败！");
+            return result;
+        }
+    }
+
+    /**
+     * 删除某同乡会某个上下车地点
+     *
+     * @param associationId
+     * @param place
+     * @return
+     */
+    @RequestMapping(value = "/deleteAssociationPlace")
+    @ResponseBody
+    public Result deleteAssociationPlace(String associationId, String place) {
+        Result result = new Result();
+
+        if (associationId == null || associationId.equals("") || place == null || place.equals("")) {
+            result.setCode(0);
+            result.setMessage("参数为空！");
+            return result;
+        }
+
+        // 查看数据库中是否有这条记录
+        Integer placeNum = associationService.findPlaceByIdAndPlace(associationId, place);
+
+        if (placeNum == 0) {
+            result.setCode(2);
+            result.setMessage("没添加过该地址或者没有此同乡会！");
+            return result;
+        }
+
+        // 删除地址
+        Integer flag = associationService.deletePlaceByIdAndPlace(associationId, place);
+
+        if (flag == 1) {
+            result.setCode(1);
+            result.setMessage("删除成功！");
+            return result;
+        } else {
+            result.setCode(-1);
+            result.setMessage("删除失败！");
+            return result;
+        }
+    }
+
+    /**
+     * 查询某同乡会的所有上下车地点
+     *
+     * @param associationId
+     * @param startIndex
+     * @param pageSize
+     * @return
+     */
+    @RequestMapping("/findAllPlaces")
+    @ResponseBody
+    public Result findAllPlaces(String associationId, Integer startIndex, Integer pageSize) {
+        Result result = new Result();
+
+        if (associationId == null || associationId.equals("") || startIndex == null || startIndex <= 0 || pageSize == null || pageSize <= 0) {
+            result.setCode(2);
+            result.setMessage("参数为空！");
+            return result;
+        }
+
+        // 查询总数量
+        Integer placeNum = associationService.findPlacesNum(associationId);
+
+        if (placeNum == 0) {
+            result.setCode(0);
+            result.setMessage("查询到0条数据！");
+            return result;
+        }
+
+        if ((startIndex - 1) * pageSize > placeNum) {
+            result.setCode(3);
+            result.setMessage("页码超出最大范围！");
+            return result;
+        }
+
+        List<String> places = associationService.findAllPlaces(associationId, (startIndex - 1) * pageSize, pageSize);
+
+        result.setCode(1);
+        result.setMessage("查询成功！");
+        Map<String, Object> map = new HashMap<>();
+        map.put("totalNum", placeNum);
+        map.put("placeList", places);
+        result.setData(map);
+        return result;
     }
 }
--- src/main/java/com/easyPoint/service/administrator/travel/AssociationService.java
+++ src/main/java/com/easyPoint/service/administrator/travel/AssociationService.java
@@ -1,5 +1,7 @@
 package com.easyPoint.service.administrator.travel;
 
+import java.util.List;
+
 /**
  * 乡会模块服务层
  *
@@ -12,4 +14,22 @@ public interface AssociationService {
 
     // 根据同乡会名称查询同乡会是否存在
     Integer findAssociationByName(String associationName);
+
+    // 根据同乡会id查询同乡会是否存在
+    Integer findAssociationById(String associationId);
+
+    // 查询某同乡会是否添加过某个上下车地点
+    Integer findPlaceByIdAndPlace(String associationId, String place);
+
+    // 添加某同乡会的上下车地点
+    Integer addPlace(String associationId, String place);
+
+    // 删除某同乡会某个上下车地点
+    Integer deletePlaceByIdAndPlace(String associationId, String place);
+
+    // 获取某同乡会的上下车地点
+    List<String> findAllPlaces(String associationId, Integer startIndex, Integer pageSize);
+
+    // 获取某同乡会的上下车地点总数量
+    Integer findPlacesNum(String associationId);
 }
