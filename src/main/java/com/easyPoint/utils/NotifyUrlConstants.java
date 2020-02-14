987bc89c7957569e9f4cc38d19b9b4d9e196c227
F2020W <525753150@qq.com>
2020-02-14 19:27:11 +0800

    租赁小程序功能1.0

--- src/main/java/com/easyPoint/utils/NotifyUrlConstants.java
+++ src/main/java/com/easyPoint/utils/NotifyUrlConstants.java
@@ -10,4 +10,7 @@ public class NotifyUrlConstants {
 
     //租车退款成功回调地址
     public static final String TOURISM_REFUND_NOTIFY_URL = "https://easypoint.club/administrator/tourism/refund.do";
+
+    //租赁商品支付成功后的通知地址
+    public static final String LEASE_NOTIFY_URL = "https://easypoint.club/miniProgram/lease/pay.do";
 }
