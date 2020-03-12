fe2f00cc166c832c4cc2987ad950abb7a6d236b5
13016029135 <1131961371@qq.com>
2020-03-12 20:16:15 +0800

    租赁初步完成

--- src/main/webapp/MiniProgram/easyPoint/pages/user/myOrder/myOrder.js
+++ src/main/webapp/MiniProgram/easyPoint/pages/user/myOrder/myOrder.js
@@ -6,20 +6,20 @@ Page({
     ruralCommittee: [], //乡会包车部分假数据
     ticket_lists: [],  //租车部分假数据
     rentItems: [//租借部分假数据
-      {
-            goodName:"随身携带可充电荧光灯带哈哈哈",
-            goodPrice:30,
-            goodDecription:"五十米长我算到这里就应该是二十了吧哈哈哈到可以",
-            goodNumber:2,
-            orderStatus:"未完成"
-      },
-      {
-            goodName: "超级好用的音响",
-            goodPrice: 150,
-            goodDecription: "蓝牙无线连接",
-            goodNumber: 1,
-            orderStatus: "押金已退还"
-      }
+      // {
+      //       goodName:"随身携带可充电荧光灯带哈哈哈",
+      //       goodPrice:30,
+      //       goodDecription:"五十米长我算到这里就应该是二十了吧哈哈哈到可以",
+      //       goodNumber:2,
+      //       orderStatus:"未完成"
+      // },
+      // {
+      //       goodName: "超级好用的音响",
+      //       goodPrice: 150,
+      //       goodDecription: "蓝牙无线连接",
+      //       goodNumber: 1,
+      //       orderStatus: "押金已退还"
+      // }
     ]
   },
   //页面加载完毕执行函数(放在首位)
@@ -57,6 +57,49 @@ Page({
       }
     })
   },
+  getRentMessage(){
+	  let that = this;
+	  let token = app.globalData.token;
+	  wx.request({
+		  url:"https://www.easypoint.club/miniProgram/lease/order",
+		  method:"GET",
+		  header: { 'content-type': 'application/x-www-form-urlencoded', token },
+		  success(res){
+			let data = res.data
+		  	switch (data.code) {
+		  	  case 200:
+				let getRentDetail = data.data
+				let rentDetail = getRentDetail.map((ele)=>{
+					let obj = {
+						goodName: ele.goodName,
+						goodPrice: ele.price,
+						goodNumber: ele.number,
+						goodDecription: ele.goodDescription||"无",
+						orderStatus: ele.state==1?"未完成":"已完成",
+						goodOrderId: ele.goodOrderId
+					}
+					return obj
+				})
+		  	    that.setData({
+		  	      rentItems: rentDetail
+		  	    })
+		  	    break;
+		  	  case 201:
+		  	    console.log("暂无订单");		  	    
+				that.setData({
+		  	      rentItems: [],
+		  	    })
+		  	    break;
+		  	  case 501:
+		  	    app.getPermission();
+		  	    break;
+		  	}
+		  },
+		  fail(err) {
+		  	console.log(err)
+		  }
+	  })
+  },
   //切换选项卡，并请求数据
   swichNav:function(e){
     if (this.data.currenTab === e.currentTarget.dataset.current) {
@@ -68,6 +111,9 @@ Page({
         this.setData({
             currenTab: type,
         })
+		if(type==2){
+			this.getRentMessage()
+		}
     }
   },
   //跳转详情数据
@@ -86,7 +132,7 @@ Page({
     let data = this.data.rentItems[index];
     wx.setStorageSync('myOrderRental', data)
     wx.navigateTo({
-        url: '/pages/user/myOrderRentDetail/myOrderRentDetail?current=2&&rentOrderId=' + data.rentOrderId,
+        url: '/pages/user/myOrderRentDetail/myOrderRentDetail?current=2&&goodOrderId=' + data.goodOrderId,
     })
   },
 })
